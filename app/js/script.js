c64884b4575329bf9492164fca926b6a7845aaec
Cristian Murrieta <20153146@aloe.ulima.edu.pe>
2021-11-24 16:45:29 -0500

    Version Final

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,5 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const btnOverlay = document.querySelector('#btnOverlay');
 const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
@@ -23,3 +24,23 @@ btnHamburger.addEventListener('click', function(){
         });
     }
 });
+
+btnOverlay.addEventListener('click', function(){
+    console.log('click overlay')
+    if(header.classList.contains('open')){ // Close menu
+        body.classList.remove('noscroll')
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    }else{ //Open menu
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+    }
+})
+
